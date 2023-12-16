7288c4a901ba4a213aa480864763aa7fb6baaeaa
Ahmet Alper <ahmetalpererdogan@gmail.com>
2023-12-16 18:44:33 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from './components/Header.jsx'
+import About from './components/About.jsx'
+import Image from './components/Image.jsx'
+import Footer from './components/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <br />
+    <About />
+    <br /><br />
+    <Image />
+    <Footer />
+    </>
+  )
+}
+
+export default App
