57bbc7a5ca650bcfe98aedae7300f6b13c78f6cb
Ahmet Alper <ahmetalpererdogan@gmail.com>
2023-12-16 18:45:29 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return (
+    
+        <h1>Ahmet Alper Erdoğan</h1>
+    )
+
+}
+
+export default Header
---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+const Image = () => {
+
+    return (
+    
+        <div>
+            <img src="https://source.unsplash.com/random/400x400?random=1" alt="image"/>
+        </div>
+    )
+
+}
+
+export default Image
